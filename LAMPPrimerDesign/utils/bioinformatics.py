3d1a8b32dbf1d1febca52c5d25832f09d26a3d9e
YiCQi <530491544@qq.com>
2024-12-03 09:58:22 +0800

    update

--- LAMPPrimerDesign/utils/bioinformatics.py
+++
@@ -1,9 +0,0 @@
-from Bio.SeqUtils import gc_fraction, MeltingTemp as mt
-
-def calculate_gc_content(sequence):
-    """计算序列的GC含量"""
-    return gc_fraction(sequence)
-
-def calculate_tm(sequence):
-    """计算引物的熔解温度"""
-    return mt.Tm_NN(sequence)
